d9af5564c831cf5445621208641d79ce107216e4
Lachlann Whitehill <lachlannwhitehill@email.local>
2026-08-02 07:50:09 +1000

    cleanup unneeded testing file

--- apps/scraper/src/scryfall/_debug-import.ts
+++
@@ -1,9 +0,0 @@
-import { runScryfallImport } from "./bulk-import.js";
-
-runScryfallImport().catch((err: any) => {
-  console.error("MSG:", err?.cause?.message ?? err?.message);
-  console.error("DETAIL:", err?.cause?.detail);
-  console.error("CONSTRAINT:", err?.cause?.constraint_name);
-  console.error("CODE:", err?.cause?.code);
-  process.exit(1);
-});
